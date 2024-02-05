e3d6dc608490f34defc77e163bcdc9d339acde13
chenglisa <97767296+chenglisa@users.noreply.github.com>
2024-02-04 18:52:57 -1000

    update parent B form

--- ParentForum.php
+++ ParentForum.php
@@ -44,291 +44,46 @@
 
             <div id="page1" class="card p-4">
                 <H1>Parent B</H1>
-                
-
+        
                 <div class="form-group row p-3">
                     <div class="col-lg-6">
-                        <label class="" hide="true" data-dm->Spouse's First Name</label>
-                        <input type="text" name="partyBFirst" class="form-control" placeholder="Spouse's Full Legal Name" data-placeholder-original="Spouse's Full Legal Name">
+                        <label class="" hide="true" data-dm->First Name</label>
+                        <input type="text" name="partyBFirst" class="form-control" placeholder="First Name" data-placeholder-original="Spouse's Full Legal Name">
                     </div>
 
                     <div class="col-lg-6">
-                        <label class="" hide="true" data-dm->Spouse's Birth or Maiden Name</label>
-                        <input type="text" name="partyBLast" class="form-control" placeholder="Spouse's Birth or Maiden Name" data-placeholder-original="Spouse's Birth or Maiden Name">
-
+                        <label class="" hide="true" data-dm->Last Name</label>
+                        <input type="text" name="partyBLast" class="form-control" placeholder="Last Name" data-placeholder-original="Spouse's Birth or Maiden Name">
                     </div>
-
                 </div>
 
                 <div class="form-group row p-3">
-                    <div class="col-lg-9">
-                        <label data-dm->Spouse's Address</label>
+                        <label data-dm->Residence</label>
                         <textarea name=" " class="form-control" placeholder="Street Address, Apt, City, State, Zip Code" data-placeholder-original="Street Address, Apt, City, State, Zip Code"></textarea>
-                    </div>
-
-                    <div class="col-lg-3">
-                        <label class="" hide="true" data-dm->Spouse's Last 4 Digits of Social Security Number</label>
-                        <input type="text" name=" " placeholder="Spouse's SSN Last 4 Digits" class="form-control" data-placeholder-original="Spouse's Last 4 Digits of Social Security Number">
-                    </div>
-
                 </div>
 
-                <div class="form-group row p-3">
+                <div class="form-group row p-4">
                     <div class="col-lg-4">
-                        <label data-dm->Spouse's Cell Phone No.</label>
+                        <label data-dm->Cell Phone</label>
                         <input type="tel" name=" " placeholder="808-888-8888" class="form-control" data-placeholder-original="808-888-8888">
                     </div>
 
                     <div class="col-lg-4">
-                        <label data-dm->Spouse's Home Phone No.</label>
-                        <input type="tel" name=" " placeholder="808-888-8888" class="form-control">
+                        <label class="" hide="true" data-dm->Email</label>
+                        <input type="email" name=" " placeholder="Email Address" class="form-control" data-placeholder-original="Spouse's Email Address">
                     </div>
 
                     <div class="col-lg-4">
-                        <label class="" hide="true" data-dm->Spouse's Email</label>
-                        <input type="email" name=" " placeholder="Spouse's Email Address" class="form-control" data-placeholder-original="Spouse's Email Address">
+                        <label class="" hide="true" data-dm->Last 4 Digits of Social Security Number</label>
+                        <input type="text" name=" " placeholder="Last 4 Digits of SSN" class="form-control" data-placeholder-original="Spouse's Last 4 Digits of Social Security Number">
                     </div>
-                </div>
-
-                <div class="form-group row p-3">
-                    <div class="col-lg-4">
-                        <label data-dm->Spouse's Date of Birth</label>
-                        <input type="date" class="form-control" lang="undefined" placeholder="" date_format="mmddyyyy" name=" ">
-                    </div>
-
-                    <div class="col-lg-4">
-                        <label data-dm->Spouse's Place of Birth</label>
-                        <input type="text" name=" " class="form-control">
-                    </div>
-
-                    <div class="col-lg-4">
-                        <label class="" hide="true" data-dm->Spouse's Race</label>
-                        <input type="text" name=" " class="form-control" placeholder="Spouse's Race" data-placeholder-original="Spouse's Race">
-                    </div>
-
                 </div>
 
             </div>
 
 
-            <div id="page5" class="card p-4">
-                <div class="form-group row p-3">
-
-                    <div class="col-lg-6">
-                        <label class="p-1" hide="true" data-dm->Spouse's Highest Grade Completed</label>
-                        <input type="text" name=" " class="form-control p-3" placeholder="Spouse's Highest Grade Completed" data-placeholder-original="Spouse's Highest Grade Completed">
-                    </div>
-
-                    <div class="col-lg-3">
-                        <label data-dm->Spouse has lived on Oahu since</label>
-                        <input type="text" name=" " class="form-control" placeholder="Year" data-placeholder-original="Year">
-                    </div>
-
-                    <div class="col-lg-3">
-                        <label data-dm->Spouse has lived in the state of Hawaii since</label>
-                        <input type="text" name=" " class="form-control" placeholder="Year" data-placeholder-original="Year">
-                    </div>
-
-                </div>
-
-                <div class="form-group">
-                    <label data-dm->Spouse's Primary Employer</label>
-                    <textarea name=" " class="form-control" placeholder="Company Name &amp; Address" data-placeholder-original="Company Name &amp; Address"></textarea>
-                </div>
-
-
-                <div class="form-group row p-3">
-
-                    <div class="col-lg-6">
-                        <label class="" hide="true" data-dm->Spouse's Job Title</label>
-                        <input type="text" name=" " class="form-control" placeholder="Spouse's Job Title" data-placeholder-original="Spouse's Job Title">
-                    </div>
-
-                    <div class="col-lg-6">
-                        <label class="" hide="true" data-dm->Spouse's Length of Service</label>
-                        <input type="text" name=" " class="form-control" placeholder="Spouse's Length of Service" data-placeholder-original="Spouse's Length of Service">
-                    </div>
-
-                </div>
-
-                <div class="form-group">
-                    <label class="" hide="true" data-dm->Spouse's Work Schedule</label>
-                    <input type="text" name=" " class="form-control" placeholder="Spouse's Work Schedule" data-placeholder-original="Spouse's Work Schedule">
-                </div>
-
-                <div class="form-group row p-2 text-center">
-
-                    <div class="col-lg-5 mx-auto">
-                        <label class="" hide="true" data-dm->Spouse's Gross Monthly Income (Primary)</label>
-                        <input type="text" name=" " class="form-control" placeholder="Spouse's Gross Monthly Income (Primary)" data-placeholder-original="Spouse's Gross Monthly Income (Primary)">
-                    </div>
-
-                </div>
-
-                <div class="form-group row p-2 text-center">
-
-                    <div class="col-lg-5 mx-auto">
-                        <label class="" hide="true" data-dm->Spouse's Gross Monthly Income (Secondary)</label>
-                        <input type="text" name=" " class="form-control" placeholder="Spouse's Gross Monthly Income (Secondary)" data-placeholder-original="Spouse's Gross Monthly Income (Secondary)">
-                    </div>
-
-                </div>
-
-                <div class="form-group row p-2 text-center">
-                    <div class="col-lg-5 mx-auto">
-                        <label class="" hide="true" data-dm->Spouse's Gross Monthly Income (Welfare)</label>
-                        <input type="text" name=" " class="form-control" placeholder="Spouse's Gross Monthly Income (Welfare)" data-placeholder-original="Spouse's Gross Monthly Income (Welfare)">
-                    </div>
-
-                </div>
-
-
-            </div>
-
-            <div id="page6" class="card p-4 ">
-                <label data-dm->Was Your Spouse Previously Married?</label>
-                <div class="">
-                    <div class="">
-                        <label class="">
-                            <input type="radio" value="Yes" name=" " class="">
-                            <span>Yes</span>
-                            <span class="">
-                                <img alt="" class="" onerror="handleImageLoadError(this)">
-                            </span>
-                        </label>
-                    </div>
-                </div>
-
-
-
-                <div class="">
-
-                    <div class="">
-                        <label class="">
-                            <input type="radio" value="No" name=" " class="">
-                            <span>No</span>
-                            <span class="">
-                                <img alt="" class="" onerror="handleImageLoadError(this)">
-                            </span>
-                        </label>
-                    </div>
-                </div>
-
-                <label data-dm->Does Your Spouse Have any Previous Civil Unions?</label>
-                <div class="">
-                    <div class="">
-                        <label class="">
-                            <input type="radio" value="Yes" name=" " class="">
-                            <span>Yes</span>
-                            <span class="">
-                                <img alt="" class="" onerror="handleImageLoadError(this)">
-                            </span>
-                        </label>
-                    </div>
-                    <div class="">
-                        <label class="">
-                            <input type="radio" value="No" name=" " class="">
-                            <span>No</span>
-                            <span class="">
-                                <img alt="" class="" onerror="handleImageLoadError(this)">
-                            </span>
-                        </label>
-                    </div>
-                </div>
-
-                <label data-dm->Is Your Spouse Pregnant?</label>
-                <div class="">
-                    <div class="">
-                        <label class="">
-                            <input type="radio" value="Yes" name=" " class="">
-                            <span>Yes</span>
-                            <span class="">
-                                <img alt="" class="" onerror="handleImageLoadError(this)">
-                            </span>
-                        </label>
-                    </div>
-                    <div class="">
-                        <label class="">
-                            <input type="radio" value="No" name=" " class="">
-                            <span>No</span>
-                            <span class="">
-                                <img alt="" class="" onerror="handleImageLoadError(this)">
-                            </span>
-                        </label>
-                    </div>
-                </div>
-
-            </div>
-
-            <div id="page7" class="card p-4">
-                <H1> Information About You Both </H1>
-
-                <div class="form-group row p-3">
-
-                    <div class="col-lg-6">
-                        <label data-dm->Date of This Marriage</label>
-                        <input type="date" class="form-control" lang="undefined" placeholder="Select Date" date_format="mmddyyyy" name=" " data-placeholder-original="Select Date">
-                    </div>
-
-                    <div class="col-lg-6">
-                        <label data-dm->Location of This Marriage</label>
-                        <input type="text" name=" " class="form-control" placeholder="County/State" data-placeholder-original="County/State">
-                    </div>
-
-                </div>
-
-                <div class="">
-                    <label data-dm->Are You Living Separately?</label>
-                    <div class="">
-                        <div class="">
-                            <label class="">
-                                <input type="radio" value="Yes" name=" " class="">
-                                <span>Yes</span>
-                                <span class="">
-                                    <img alt="" class="" onerror="handleImageLoadError(this)">
-                                </span>
-                            </label>
-                        </div>
-                        <div class="">
-                            <label class="">
-                                <input type="radio" value="No" name=" " class="">
-                                <span>No</span>
-                                <span class="">
-                                    <img alt="" class="" onerror="handleImageLoadError(this)">
-                                </span>
-                            </label>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="">
-                    <label data-dm->Do either you or your spouse have any children?</label>
-                    <div class="">
-                        <div class="">
-                            <label class="">
-                                <input type="radio" value="Yes" name=" " class="">
-                                <span>Yes</span>
-                                <span class="">
-                                    <img alt="" class="" onerror="handleImageLoadError(this)">
-                                </span>
-                            </label>
-                        </div>
-                        <div class="">
-                            <label class="">
-                                <input type="radio" value="No" name=" " class="">
-                                <span>No</span>
-                                <span class="">
-                                    <img alt="" class="" onerror="handleImageLoadError(this)">
-                                </span>
-                            </label>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
-            <div id="page8" class="card p-4">
-                <H1>Info about your Kids</H1>
+            <div id="page2" class="card p-4">
+                <H1>Children</H1>
 
                 <div class="form-group">
                 <label data-dm->Name, Age, Grade, School Name of Each Child</label>
