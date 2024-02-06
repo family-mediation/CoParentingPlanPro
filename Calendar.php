fa7c3ec47e627ddc8d3bc9b560670f4ef3c81dab
willjsimmons <willjsimmons@gmail.com>
2024-02-05 17:59:24 -1000

    basic calendar generation proof of concept iscomplete.

--- Calendar.php
+++ Calendar.php
@@ -1,17 +1,9 @@
-<? php
+<?php
     //open the file
-    
-    if(isset($_POST['partyAName']))
-    {
-        $fileName = $_POST['partyAName']."calendar.ics";
-    }
-    else
-    {
-        $fileName = "testCalendar.ics";
-    }
+    $directory = "documents/";
+    $fileName = $directory."testCalendar.ics";
     $calendarFile = fopen($fileName, "w");
     //generate the calendar string.
-    
     $calendarHeader = "BEGIN:VCALENDAR";
     $calendarEvent = "   BEGIN:VEVENT
             DTSTART;TZID=Pacific/Honolulu:20150901T120000
@@ -24,10 +16,10 @@
             LOCATION:Pacific Ocean Science & Technology\, Honolulu\, HI 96822\, USA
             SEQUENCE:1
             STATUS:CONFIRMED
-            SUMMARY:ICS 111 Lab
+            SUMMARY:".$fileName."
             TRANSP:OPAQUE
-            END:VEVEN
-            "
+            END:VEVENT
+            ";
     $calendarString = $calendarHeader . $calendarEvent; // this will eventually have multiple events in a loop.    
     fwrite($calendarFile, $calendarString);
     fclose($calendarFile);
@@ -42,7 +34,7 @@ require_once("views/header.php");
 ?>
 <row class="text-center">
     <H1>Heres your generated Calendar</H1>
-    <p><?php echo $fileName; ?></p>
+    <p><a href="<?php echo $fileName; ?>">click</a> to download <?php echo $fileName; ?></p>
 </row>
 <?php
 require_once("views/footer.php");
