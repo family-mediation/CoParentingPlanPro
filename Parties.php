f2dd0223bb1a874a27eed174ee5ce6b86c94f044
Yeeun Shin <yeeun2@hawaii.edu>
2024-02-05 15:18:10 -1000

    Update Parties.php

---
+++ Parties.php
@@ -0,0 +1,58 @@
+<div class="text-center">
+    <form method="post" class="" locale="ENGLISH" action="Co-Parenting Plan Template.php">
+        <div id="mainForm">
+        <div id="page1" class="container card p-4">
+                <H1> Information on Parent A</H1>
+                <h2>Please enter your full legal Name and residence address</h2>
+                <div class="form-group row p-3">
+                    <div class="col-lg-6">
+                        <label class="" hide="true" data-dm->First Name</label>
+                        <input type="text" class="form-control" name="partyAFirst" placeholder="First Name" data-placeholder-original="Full Legal Name">
+                    </div>
+
+                    <div class="col-lg-6">
+                        <label class="" hide="true" data-dm->Last Name</label>
+                        <input type="text" class="form-control" name="partyALast" placeholder="Last Name" data-placeholder-original="Full Legal Name">
+                    </div>    
+                </div>
+
+                <div class="form-group row p-3">
+                    <div class="col-lg-9">
+                        <label data-dm->Your Address</label>
+                        <textarea name="partyAStreet" class="form-control" placeholder="Street Address, Apt, City, State, Zip Code" data-placeholder-original="Street Address, Apt, City, State, Zip Code"></textarea>
+                    </div>
+                </div>
+
+            </div>
+
+            <div id="page2" class="container card p-4">
+                <H1> Information on Parent B</H1>
+                <h2>Please enter your full legal Name and residence address</h2>
+                <div class="form-group row p-3">
+                    <div class="col-lg-6">
+                        <label class="" hide="true" data-dm->First Name</label>
+                        <input type="text" class="form-control" name="partyAFirst" placeholder="First Name" data-placeholder-original="Full Legal Name">
+                    </div>
+
+                    <div class="col-lg-6">
+                        <label class="" hide="true" data-dm->Last Name</label>
+                        <input type="text" class="form-control" name="partyALast" placeholder="Last Name" data-placeholder-original="Full Legal Name">
+                    </div>    
+                </div>
+
+                <div class="form-group row p-3">
+                    <div class="col-lg-9">
+                        <label data-dm->Your Address</label>
+                        <textarea name="partyAStreet" class="form-control" placeholder="Street Address, Apt, City, State, Zip Code" data-placeholder-original="Street Address, Apt, City, State, Zip Code"></textarea>
+                    </div>
+                </div>
+                
+            </div>
+
+
+        </div>
+    </form>
+    <div class="row justify-content-center pt-4 pb-4">
+        <button id="previous" onClick="previousHandler()">Previous Page</button> &nbsp;&nbsp; <button id="next" onclick="nextHandler()">Next Page</button>
+    </div>
+</div>
