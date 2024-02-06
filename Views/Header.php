ce4f0d0012ae022977e7d3b9d1e2910525262967
Yeeun Shin <yeeun2@hawaii.edu>
2024-02-05 16:12:46 -1000

    edits

--- Views/Header.php
+++ Views/Header.php
@@ -31,9 +31,14 @@
                     <li class="dropdown-item"><a class="nav-link">About your children</a></li>
                 </div>
             </div>
-
             <li class="nav-item"><a class="nav-link"> Section 2: <br /> Guiding Principles </a></li>
-            <li class="nav-item"><a class="nav-link"> Section 3: <br /> Parties: </a></li>
+            <div class="dropdown">
+                <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown"> Section 3: Parties </a>
+                <div class="dropdown-menu">
+                    <li class="dropdown-item"><a class="nav-link">Parent A</a></li>
+                    <li class="dropdown-item"><a class="nav-link">Parent B</a></li>
+                </div>
+            </div>
             <li class="nav-item"><a class="nav-link"> Section 4: <br /> Children </a></li>
             <li class="nav-item"><a class="nav-link"> Section 5: <br /> Legal Custody </a></li>
             <li class="nav-item"><a class="nav-link"> Section 6: <br /> Physical Custody and Timesharing </a></li>
