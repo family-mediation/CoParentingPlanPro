a620423c5acbf89d46e5ccbdd382f603918fd4ee
willjsimmons <willjsimmons@gmail.com>
2024-02-05 18:48:21 -1000

    Ical file is correctly generated and can be imported into a calendar

--- Calendar.php
+++ Calendar.php
@@ -4,23 +4,41 @@
     $fileName = $directory."testCalendar.ics";
     $calendarFile = fopen($fileName, "w");
     //generate the calendar string.
-    $calendarHeader = "BEGIN:VCALENDAR";
-    $calendarEvent = "   BEGIN:VEVENT
-            DTSTART;TZID=Pacific/Honolulu:20150901T120000
-            DTEND;TZID=Pacific/Honolulu:20150901T131500
-            RRULE:FREQ=WEEKLY;UNTIL=20151229T220000Z;BYDAY=TU,TH
-            DTSTAMP:20240206T030807Z
-            UID:r79kfboffhrmncef3ca2fe4sqk@google.com
-            CREATED:20150830T020031Z
-            LAST-MODIFIED:20150830T020131Z
-            LOCATION:Pacific Ocean Science & Technology\, Honolulu\, HI 96822\, USA
-            SEQUENCE:1
-            STATUS:CONFIRMED
-            SUMMARY:".$fileName."
-            TRANSP:OPAQUE
-            END:VEVENT
-            ";
-    $calendarString = $calendarHeader . $calendarEvent; // this will eventually have multiple events in a loop.    
+    $calendarHeader = "BEGIN:VCALENDAR
+PRODID:-//Google Inc//Google Calendar 70.9054//EN
+VERSION:2.0
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+X-WR-CALNAME:wjs7@hawaii.edu
+X-WR-TIMEZONE:Pacific/Honolulu
+BEGIN:VTIMEZONE
+TZID:Pacific/Honolulu
+X-LIC-LOCATION:Pacific/Honolulu
+BEGIN:STANDARD
+TZOFFSETFROM:-1000
+TZOFFSETTO:-1000
+TZNAME:HST
+DTSTART:19700101T000000
+END:STANDARD
+END:VTIMEZONE
+";
+    $calendarEvent = "BEGIN:VEVENT
+DTSTART;TZID=Pacific/Honolulu:20150901T120000
+DTEND;TZID=Pacific/Honolulu:20150901T131500
+RRULE:FREQ=WEEKLY;UNTIL=20151229T220000Z;BYDAY=TU,TH
+DTSTAMP:20240206T030807Z
+UID:r79kfboffhrmncef3ca2fe4sqk@google.com
+CREATED:20150830T020031Z
+LAST-MODIFIED:20150830T020131Z
+LOCATION:Pacific Ocean Science & Technology\, Honolulu\, HI 96822\, USA
+SEQUENCE:1
+STATUS:CONFIRMED
+SUMMARY: simpleEvent
+TRANSP:OPAQUE
+END:VEVENT
+";
+    $calendarFooter ="END:VCALENDAR";
+    $calendarString = $calendarHeader . $calendarEvent . $calendarFooter; // this will eventually have multiple events in a loop.    
     fwrite($calendarFile, $calendarString);
     fclose($calendarFile);
     // 
@@ -33,8 +51,10 @@
 require_once("views/header.php");
 ?>
 <row class="text-center">
-    <H1>Heres your generated Calendar</H1>
-    <p><a href="<?php echo $fileName; ?>">click</a> to download <?php echo $fileName; ?></p>
+    <div id="page0" class="card p-4">
+        <H1>Heres your generated Calendar</H1>
+        <p><a href="<?php echo $fileName; ?>">click</a> to download <?php echo $fileName; ?></p>
+    </div>
 </row>
 <?php
 require_once("views/footer.php");
