f87e7853984e6214fceaeb79c095bccfeccad47e
willjsimmons <willjsimmons@gmail.com>
2024-02-14 20:50:45 -1000

    Strides have been made on the document generation front.

--- DocumentGenerator.php
+++ DocumentGenerator.php
@@ -3,14 +3,17 @@ abstract class documentGenerator
 {
 	public $fileName;
 	public $responses;
-	public $fileOutputString;
+	public $fileOutput;
+	public $fileContentString;
+	public $tableOfContentsString;
 	public function __construct(string $fileName,array $responses)
  	{
 		$this->fileName = $fileName;
 		$this->responses = $responses;
-		$this->fileOutputString = fopen($fileName, "w");
+		$this->fileOutput = fopen("./".$fileName,"w+");
  	}
 // Generate the different components.
+	public abstract function genHeader();
 	public abstract function genPart1();
 	public abstract function genPart2();
 	public abstract function genPart3();
@@ -18,9 +21,12 @@ abstract class documentGenerator
 	public abstract function genPart5();
 	public abstract function genPart6();
 	public abstract function genPart7();
+	public abstract function genFooter();
 	public abstract function packageDocument();
 	public function generateDocument()
 	{
+		$this->genHeader();
+		$this->genPart1();
 		$this->genPart1();
 		$this->genPart2();
 		$this->genPart3();
@@ -28,44 +34,8 @@ abstract class documentGenerator
 		$this->genPart5();
 		$this->genPart6();
 		$this->genPart7();
+		$this->genFooter();
 		$this->packageDocument();
+		fclose($this->fileOutput);
 	}
 }
-
-class wordDocGenerator extends documentGenerator
-{
-	function genPart1()
-	{
-        echo "Generating Part1 <br/>";
-	}
-	function genPart2()
-	{
-         echo "Generating Part2 <br/>";
-	}
-    function genPart3()
-	{
-         echo "Generating Part3 <br/>";
-	}
-    function genPart4()
-	{
-         echo "Generating Part4 <br/>";
-	}
-	function genPart5()
-	{
-         echo "Generating Part5 <br/>";
-	}
-	function genPart6()
-	{
-          echo "Generating Part6 <br/>";
-	}
-	function genPart7()
-	{
-          echo "Generating Part7 <br/>";
-	}
-	function packageDocument()
-	{
-
-	}
-}
-$wordGen = new wordDocGenerator("output.docx",array(1,2,3));
-$wordGen->generateDocument();
---
+++ WordDocGenerator.php
@@ -0,0 +1,54 @@
+<?php
+require_once("DocumentGenerator.php");
+class wordDocGenerator extends documentGenerator
+{
+	function genHeader()
+	{
+	    echo "Generating Header " . $this->fileName . " <br/>";
+	   $this->fileContentString .= "test test test";
+	}
+	function genFooter()
+	{
+	echo "Generating Footer " . $this->fileName . " <br/>";
+	    $this->fileContentString .= "test test test";
+	}
+	function genPart1()
+	{
+        echo "Generating Part1 " . $this->fileName . " <br/>";
+        $this->fileContentString .= "test test test";
+	}
+	function genPart2()
+	{
+         echo "Generating Part2 <br/>";
+	}
+    function genPart3()
+	{
+         echo "Generating Part3 <br/>";
+	}
+    function genPart4()
+	{
+         echo "Generating Part4 <br/>";
+	}
+	function genPart5()
+	{
+         echo "Generating Part5 <br/>";
+	}
+	function genPart6()
+	{
+          echo "Generating Part6 <br/>";
+	}
+	function genPart7()
+	{
+          echo "Generating Part7 <br/>";
+	}
+	function packageDocument()
+	{
+	// Take the template.zip copy it and rename it in the downloads folder
+	// Take the string and write it to document.xml
+	 fwrite($this->fileOutput,$this->fileContentString);
+	// Zip the folder and rename it to .docx
+	// Serve the .docx
+	}
+}
+$wordGen = new wordDocGenerator("output.txt",array(1,2,3));
+$wordGen->generateDocument();
