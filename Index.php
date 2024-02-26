cbcdb06358140e69b472d99b443aa97f23e138aa
willjsimmons <willjsimmons@gmail.com>
2024-02-26 13:45:51 -1000

    Update index

--- Index.php
+++ Index.php
@@ -4,5 +4,27 @@ require_once("MysqlUtil.php");
 // create an object that will serve as our database.
 $db = new Database; 
 require_once("views/Header.php");
-require_once("ParentForum.php");
+$page = $_GET['page'];
+switch($page)
+{
+    case 0:
+        require_once("ParentForum.php");
+        break;
+    case 1:
+        require_once("Children.php");
+        break;
+    case 2:
+        require_once("ParentForum.php");
+        break;
+    case 3:
+        require_once("ParentForum.php");
+        break;
+    case 4:
+        require_once("ParentForum.php");
+        break;
+    case 5:
+        require_once("ParentForum.php");
+        break;
+
+}
 require_once("views/Footer.php");
