cebed73451f56ce696cd57a1dcfa038842ffd700
willjsimmons <willjsimmons@gmail.com>
2024-02-26 18:42:03 -1000

    response functionality is working
    whenever a form is submitted within the session the submitted values are appended to the responses array in the session variable.

--- Index.php
+++ Index.php
@@ -1,16 +1,19 @@
 <?php
 session_start();
-$responses = array();
+if(!isset($_SESSION['responses']))
+{
+$_SESSION['responses'] = array();
+}
 if(isset($_POST))
 {
-$responses += $_POST;
+$_SESSION['responses'] += $_POST;
 }
 require_once("MysqlUtil.php");
 // create an object that will serve as our database.
 $db = new Database;
 $page = isset($_GET['page']) ?$_GET['page'] : 1 ;
 require_once("views/Header.php");
-echo var_dump($responses);
+echo var_dump($_SESSION['responses']);
 switch($page)
 {
     case 1:
--- Submit.php
+++ Submit.php
@@ -3,17 +3,18 @@ require_once("documentGenerators/HtmlGenerator.php");
 require_once("documentGenerators/WordDocGenerator.php");
 require_once("documentGenerators/CalendarGenerator.php");
 require_once("views/Header.php");
-$fileName = $_POST["partyALast"]." ".$_POST["partyBLast"];
+$src = $_SESSION['responses'];
+$fileName = $src["partyALast"]." ".$src["partyBLast"];
 $filePath = "./documents/" . $fileName;
 //later check for E_warning to see if directory already exists.
 if(!is_dir($filePath))
 {
     mkdir($filePath);
  }
-$htmlGen = new HtmlGenerator($filePath.'/'.$fileName.".html",$_POST);
+$htmlGen = new HtmlGenerator($filePath.'/'.$fileName.".html",$src);
 $htmlGen->generateDocument();
-$wordGen = new wordDocGenerator($filePath.'/'.$fileName.".docx",$_POST);
+$wordGen = new wordDocGenerator($filePath.'/'.$fileName.".docx",$src);
 $wordGen->generateDocument();
-$calGen = new CalendarGenerator($filePath.'/'.$fileName.".ics",$_POST);
+$calGen = new CalendarGenerator($filePath.'/'.$fileName.".ics",$src);
 $calGen->generateDocument();
 require_once("views/Footer.php");
