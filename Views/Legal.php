49bf7980b8add37174713d9b20ab699351d435c0
michho8 <mh888@hawaii.edu>
2024-04-03 11:52:42 -1000

    Create Legal page

--- Views/Legal.php
+++ Views/Legal.php
@@ -3,8 +3,41 @@
     <div id="mainForm">
       <div id="page0" class="container card p-4">
         <h1>Legal</h1>
-        <p>place your code here</p>
+        <div style="text-align:left">
+        <h3>Continuing Jurisdiction</h3>
+          <p>Except as otherwise limited by Statute, Court Rule or case authority, the Family Court shall have the ongoing authority and personal and subject matter jurisdiction to make any other just and equitable orders which may be necessary to enforce the intended agreement and specific provisions set forth herein.</p>
 
+        <h3>Enforcement</h3>
+          <p>A Parent who fails to comply with this Co-Parenting Plan: Agreement Regarding Child Custody, Timesharing, and Support shall be liable to the other Parent for all reasonable legal fees and costs incurred and reasonable damages suffered by the other Parent as a result of noncompliance.  The Family Court shall have continuing jurisdiction over the parents and their property to enforce and implement the provisions of the Agreement, to the extent permitted by applicable statutory authority, court rule and/or case law.</p>
+        <h3>Voluntary Execution</h3>
+          <p>Each Parent acknowledges that they have voluntarily executed this Co-Parenting Plan: Agreement Regarding Child Custody, Timesharing, and Support with sufficient knowledge of the facts and the law, and that it is fair and reasonable.  Both Parents have been advised to seek independent legal counsel regarding these and all other matters contained herein.</p>
+        </div>
+        <div class="row justify-content-center">
+          <div class="column p-5">
+            <div class="row">
+            <label style="font-size: x-large" hide="true" data-dm->Parent 1 E-Signature
+              <input type="text" class="form-control" name="partyASignature" placeholder="Sign full name here" required>
+            </label>
+            </div>
+            <div class="row">
+            <label style="font-size: x-large" hide="true" data-dm->Date
+              <input type="date" class="form-control" name="partyADateSigned" required>
+            </label>
+            </div>
+          </div>
+          <div class="column p-5">
+            <div class="row">
+            <label style="font-size: x-large" hide="true" data-dm->Parent 2 E-Signature
+              <input type="text" class="form-control" name="partyBSignature" placeholder="Sign full name here" required>
+            </label>
+            </div>
+          <div class="row">
+            <label style="font-size: x-large" hide="true" data-dm->Date
+              <input type="date" class="form-control" name="partyBDateSigned" required>
+            </label>
+          </div>
+          </div>
+        </div>
         <div class="row justify-content-center pt-4 pb-4">
           <button id="generate" name="generatePlan" type="submit" value="Generate Plan">Generate Plan</button>
         </div>
