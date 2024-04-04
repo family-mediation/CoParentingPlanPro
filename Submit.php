57871ffbf855b9e9dbbd8915bcc5dde97e6d0a02
willjsimmons <willjsimmons@gmail.com>
2024-04-03 19:11:28 -1000

    Update submit with basic zip creation

--- Submit.php
+++ Submit.php
@@ -6,6 +6,7 @@ require_once("views/Header.php");
 $src = $_SESSION['responses'];
 $fileName = $src["partyALast"]." ".$src["partyBLast"];
 $filePath = "./documents/" . $fileName;
+$zipFilePath = $filePath . ".zip";
 //later check for E_warning to see if directory already exists.
 if(!is_dir($filePath))
 {
@@ -15,7 +16,15 @@ $htmlGen = new HtmlGenerator($filePath.'/'.$fileName.".html",$src);
 $htmlGen->generateDocument();
 $wordGen = new wordDocGenerator($filePath.'/'.$fileName.".docx",$src);
 $wordGen->generateDocument();
-$calGen = new CalendarGenerator($filePath.'/'.$fileName.".ics",$src);
-$calGen->generateDocument();
-require_once("views/Footer.php");
+//$calGen = new CalendarGenerator($filePath.'/'.$fileName.".ics",$src);
+//$calGen->generateDocument();
 unset($_SESSION['responses']);
+$documents = new ZipArchive();// add error testing
+if($documents->open($zipFilePath,ZipArchive::CREATE) != true)
+{
+  exit("unable to create zip folder ".$zipFilePath);
+}
+$documents->addGlob($filePath . '/*');
+$documents->close();
+echo "<a href=\"".$zipFilePath."\" >Download Your Folder </a>";
+require_once("views/Footer.php");
