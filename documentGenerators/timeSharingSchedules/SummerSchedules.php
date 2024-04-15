25a2f95a76039c229dd1274f37639a676afc5b00
michho8 <mh888@hawaii.edu>
2024-04-15 13:15:42 -1000

    Get rid of php echo in timeSharingSchedules

--- documentGenerators/timeSharingSchedules/SummerSchedules.php
+++ documentGenerators/timeSharingSchedules/SummerSchedules.php
@@ -4,10 +4,10 @@ class SummerSchedules {
         return "<p class=MsoNormal style='margin-top:0in;margin-right:1.8pt;margin-bottom:.05pt;
             margin-left:0in;text-align:justify;text-indent:1.5in;line-height:150%'><a
             name=PhysicalCustodySummerBreakSB><b>6.02E(i)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </b><u>SPLIT BREAK</u></a>.&nbsp;
-            In odd years, <?php echo $parentAFirst; ?> shall have the Child(ren) during the first half of the
-            break and <?php echo $parentBFirst; ?> shall have the Child(ren) during the second half of the
-            break.&nbsp; In even years, <?php echo $parentBFirst; ?> shall have the Child(ren) during the first half
-            of the break and <?php echo $parentAFirst; ?> shall have the Child(ren) during the second half of
+            In odd years, $parentAFirst shall have the Child(ren) during the first half of the
+            break and $parentBFirst shall have the Child(ren) during the second half of the
+            break.&nbsp; In even years, $parentBFirst shall have the Child(ren) during the first half
+            of the break and $parentAFirst shall have the Child(ren) during the second half of
             the break, to include New Year's Eve and New Year's Day.</p>";
     }
     public function get_continue(): string {
@@ -26,11 +26,11 @@ class SummerSchedules {
             SCHEDULE DURING THE SCHOOL YEAR WITH OPTION FOR UNITERRUPTED TIME</u></a>.&nbsp;
             Parents shall continue the normal during the school year schedule during this
             break provided however that both parents shall have the option to take
-             <?php echo $days_weeks; ?>days/weeks of uninterrupted time with the Child(ren) every summer.&nbsp; Parents
+            $days_weeks days/weeks of uninterrupted time with the Child(ren) every summer.&nbsp; Parents
             shall select and exchange their dates for uninterrupted time with the
-            Child(ren) by no later than  <?php echo $no_later_than; ?>(date) of the same year.&nbsp; In even
-            years, <?php echo $parentAFirst; ?> shall have the first choice of dates for uninterrupted time
-            during the Summer Break.&nbsp; In odd years, <?php echo $parentBFirst; ?> shall have the first choice of
+            Child(ren) by no later than $no_later_than (date) of the same year.&nbsp; In even
+            years, $parentAFirst shall have the first choice of dates for uninterrupted time
+            during the Summer Break.&nbsp; In odd years, $parentBFirst shall have the first choice of
             dates for uninterrupted time during the Summer Break.&nbsp; </p>";
     }
 }
